67968a47a9ab11f0d3c588c319fe05abb76f6cff
unknown <monika.pikuzinska01@gmai.com>
2020-03-19 14:00:18 +0100

    Updates

---
+++ precache-manifest.0621748769c4684672887183f1f7f5ec.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d8a5951fc6e682123c0c7b1a441d47d",
+    "url": "/quiz-throne-of-glass/index.html"
+  },
+  {
+    "revision": "06abc3255ab3a8b13b80",
+    "url": "/quiz-throne-of-glass/static/css/main.51c12ec3.chunk.css"
+  },
+  {
+    "revision": "42503a6a457fba2727a0",
+    "url": "/quiz-throne-of-glass/static/js/2.775dc776.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/quiz-throne-of-glass/static/js/2.775dc776.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06abc3255ab3a8b13b80",
+    "url": "/quiz-throne-of-glass/static/js/main.c1012e5a.chunk.js"
+  },
+  {
+    "revision": "b2211bf2de3583bcfba1",
+    "url": "/quiz-throne-of-glass/static/js/runtime-main.371de2c9.js"
+  },
+  {
+    "revision": "3aa11b464c9a5aa028a32d9a935b676b",
+    "url": "/quiz-throne-of-glass/static/media/crown.3aa11b46.svg"
+  },
+  {
+    "revision": "843e0fb8c84845a14bb1164225b67b76",
+    "url": "/quiz-throne-of-glass/static/media/forest.843e0fb8.svg"
+  },
+  {
+    "revision": "9151356440b671eb2aebec3771de1999",
+    "url": "/quiz-throne-of-glass/static/media/hawk.91513564.svg"
+  }
+]);
