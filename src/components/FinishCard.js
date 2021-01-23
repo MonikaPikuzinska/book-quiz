c07c063e20a40c596c9b52c2e5a25d5ab8e06add
unknown <monika.pikuzinska01@gmai.com>
2021-01-23 11:50:51 +0100

    alignment of code

--- src/components/FinishCard.js
+++ src/components/FinishCard.js
@@ -4,18 +4,18 @@ import { quizData } from '../questionsData';
 function FinishCard(props) {
   return (
     <>
-    <h3 className="result_count">Game Over! Your final score is {props.counter} {props.counter===1? 'point': 'points'} </h3>
-    <div className="result_container-for-right-answers">
-            The correct answers:
-            <ul className="result_list-right-answers">
-              {quizData.map((item, index) => (
-                <li className="result_right-answers" key={index}>
-                  {item.answer}
-                </li>
-              ))}
-            </ul>
-          </div>
-      </>
+      <h3 className="result_count">Game Over! Your final score is {props.counter} {props.counter===1? 'point': 'points'} </h3>
+      <div className="result_container-for-right-answers">
+        The correct answers:
+        <ul className="result_list-right-answers">
+          {quizData.map((item, index) => (
+           <li className="result_right-answers" key={index}>
+              {item.answer}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
