f235eabeed408661b379e16d60e6f82a889f10b8
MonikaPikuzinska <monika.pikuzinska01@gmail.com>
2021-02-03 10:26:30 +0100

    updated img

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,30 +2,32 @@ import React from 'react';
 import crown from '../img/crown.svg';
 import harry from '../img/harry-potter.svg';
 import twilight from '../img/twilight.svg';
-import hunger from '../img/hunger.png';
+import hunger from '../img/hunger.svg';
 
 const HomePage = () => {
     return ( 
         <>
-            <div className="grid grid-flow-col grid-flow-row grid-cols-4 grid-rows-3 bg-gray-200">
-                <button className="h-12 border border-indigo-500 text-indigo-500 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:text-white hover:bg-indigo-600 focus:outline-none focus:shadow-outline row-start-0 row-end-0 justify-self-end gap-1 col-end-4 col-start-4">en</button>
-                <button className="w-10 h-12 gap-1 justify-self-end row-start-0 row-end-0 col-end-4 col-start-4 bg-blue-400 row-span-1">pl</button>
-                <h1>Book quizzes</h1>
-                <div>
-                    <title>Throne of glass</title>
-                    <img src={crown}/>
-                </div>
-                <div>
-                    <title>Harry Potter</title>
-                    <img src={harry}/>
-                </div>
-                <div>
-                    <title>Twilight</title>
-                    <img src={twilight}/>
-                </div>
-                <div>
-                    <title>The Hunger Games</title>
-                    <img src={hunger}/>
+            <div className="flex flex-col">
+                <button className="italic font-bold h-12 w-12 border border-gray-700 text-gray-700 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:text-white hover:bg-gray-800 focus:outline-none focus:shadow-outline">en</button>
+                <button className="italic font-bold h-12 w-12 border border-gray-700 text-gray-700 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:text-white hover:bg-gray-800 focus:outline-none focus:shadow-outline">pl</button>
+                <h1 className="p-4 text-center self-center block italic font-bold text-8xl text-gray-700">Book quizzes</h1>
+                <div className="p-20 flex flex-wrap flex-row space-y-14 space-x-14">
+                    <div className="flex-1 self-center shadow-2xl cursor-pointer hover:scale-110 transform transition duration-500 w-100 h-150 flex flex-col">
+                        <img className="bg-green-400 p-8" src={crown}/>
+                        <p className="self-center block p-3 text-green-400 text-2xl font-bold">Throne of glass</p>
+                    </div>
+                    <div className="flex-1 self-center shadow-2xl cursor-pointer hover:scale-110 transform transition duration-500 w-100 h-150 flex flex-col">
+                        <img className="bg-indigo-400 p-8" src={harry}/>
+                        <p className="self-center block p-3 text-indigo-400 text-2xl font-bold">Harry Potter</p>
+                    </div>
+                    <div className="flex-1 self-center shadow-2xl cursor-pointer hover:scale-110 transform transition duration-500 w-100 h-150 flex flex-col">
+                        <img className="bg-gray-400 p-8" src={twilight}/>
+                        <p className="self-center block p-3 text-gray-400 text-2xl font-bold">Twilight</p>
+                    </div>
+                    <div className="flex-1 self-center shadow-2xl cursor-pointer hover:scale-110 transform transition duration-500 w-100 h-150 flex flex-col">
+                        <img className="bg-red-400 p-8" src={hunger}/>
+                        <p className="self-center block p-3 text-red-400 text-2xl font-bold">The Hunger Games</p>
+                    </div>
                 </div>
             </div>
         </>
