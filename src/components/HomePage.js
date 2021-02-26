4f5dea0663c2b7d6a7fb390d4bb0a549b07953c7
MonikaPikuzinska <monika.pikuzinska01@gmail.com>
2021-02-26 15:41:35 +0100

    fixed display quiz, added tailwindcss, added check answer

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -19,10 +19,10 @@ const HomePage = () => {
                 <LangBtn lang={'pl'} langf={pl}/>
                 <h1 className="p-4 text-center self-center block italic font-bold text-8xl text-gray-700">{t('welcome.title')}</h1>
                 <div className="p-20 flex flex-wrap flex-row justify-around">
-                    <BookCard color={'green'} img={Crown} title={'throne'} funName={throne} funColor={green}/>
-                    <BookCard color={'indigo'} img={Harry} title={'harry'} funName={harry} funColor={indigo}/>
-                    <BookCard color={'gray'} img={Twilight} title={'twilight'} funName={twilight} funColor={gray}/>
-                    <BookCard color={'red'} img={Hunger} title={'hunger'} funName={hunger} funColor={red}/>
+                    <BookCard color={'green'} img={Crown} lin={'throne'} title={'Throne of glass'} funName={throne} funColor={green}/>
+                    <BookCard color={'indigo'} img={Harry} lin={'harry'} title={'Harry Potter'} funName={harry} funColor={indigo}/>
+                    <BookCard color={'gray'} img={Twilight} lin={'twilight'} title={'Twilight'} funName={twilight} funColor={gray}/>
+                    <BookCard color={'red'} img={Hunger} lin={'hunger'} title={'The Hunger Games'} funName={hunger} funColor={red}/>
                 </div>
             </div>
         </>
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,12 +2,16 @@ import React, { useState, useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import {quizData} from '../questionsData';
 import FinishCard from './FinishCard';
+import { Link } from 'react-router-dom'; 
+import {useTranslation} from "react-i18next";
 
 function Quiz() {
+    const {t, i18n} = useTranslation('common');
+
     const book = useSelector(state => state.book);
     const color = useSelector(state => state.color);
     const lang = useSelector(state => state.lang);
-    
+
     const [currentQuestion, setCurrentQuestion] = useState(0);
     const [options, setOptions] = useState([]);
     const [questions, setQuestions] = useState('');
@@ -15,17 +19,15 @@ function Quiz() {
     const [myAnswer, setMyAnswer] = useState(0);
     const [counter, setCounter] = useState(0);
     const [isFinished, setIsFinished] = useState(false);
-    const [quiz, setQuiz] = useState(book)
-    const [quizLang, setQuizLang] = useState(lang)
+    const [rightAns, setRightAns] = useState(false);
 
-    const x = quizData.$(book)[currentQuestion].question.$(lang);
+    const bookFound = quizData.find(bookObj => bookObj.name === book);
     const loadQuizData = () => {
-        setQuestions(x);
-        setAnswer(quizData[quiz][currentQuestion].answer);
-        setOptions(quizData[0].questions[currentQuestion].options[quizLang])
+        setQuestions(bookFound.questions[currentQuestion].question[lang]);
+        setAnswer(bookFound.questions[currentQuestion].answer);
+        setOptions(bookFound.questions[currentQuestion].options[lang])
     };
-    console.log(options)
-    console.log(quizData.Twilight[4].options.en)
+    
     const handleChangeQuestion = () => {
         if(myAnswer===answer){
           setCounter(counter+1);
@@ -43,8 +45,17 @@ function Quiz() {
         }
     });
 
-    const handleCheckAnswer = (myAnswer) => {
+    const handleCheckAnswer = (myAnswer, i) => {
         setMyAnswer(options.indexOf(myAnswer));
+        if (myAnswer===answer){
+            let element = document.getElementById(i);
+            element.classList.toggle("bg-green-200");
+        } else {
+            let element = document.getElementById(i);
+            element.classList.toggle("bg-red-200");
+            let element2 = document.getElementById(answer);
+            element2.classList.toggle("bg-green-200");
+        };
     };
 
     const handleFinish = ()=>{
@@ -56,18 +67,24 @@ function Quiz() {
         (isFinished)? 
             <FinishCard counter={counter}/>
          :<>        
-            <h2 className="quiz-content_question-counter">{currentQuestion===0? `Question`: `Questions`}  {currentQuestion +1}  out of {quizData.length} remaining </h2>
-            <h1 className="quiz-content_question">{questions} </h1>
-            {options.map(option => (
-            <div onClick={()=>handleCheckAnswer(option)} className={myAnswer === option ? "quiz-content_options selected" : "quiz-content_options"} >{option}</div>
-            ))}
-            {currentQuestion<quizData.length-1 ?
-            <button className="quiz-content_btn"
-                onClick={handleChangeQuestion}
-              >
-                Next
-            </button> :
-            <button className="quiz-content_btn" onClick={handleFinish}>Finish</button>}
+            <div className="flex flex-col">
+                <button className="italic flex-grow-0 w-36 font-bold border border-gray-700 text-gray-700 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:text-white hover:bg-gray-800 focus:outline-none focus:shadow-outline"><Link to={'/'}>{t(`quiz.main`)}</Link></button>
+                <h2 className={`p-4 text-center self-center block italic font-bold text-8xl text-${color}-400`}>{t(`welcome.${book}`)}</h2>
+                <div className="flex flex-col self-center p-8 m-8 shadow-2xl">
+                    <h2>{currentQuestion +1}  / {bookFound.questions.length} </h2>
+                    <h1 className="text-center p-2 text-xl block italic font-bold">{questions} </h1>
+                    {options.map((option, i)=> (
+                    <div id={i} onClick={()=>handleCheckAnswer(option, i)} className={rightAns ? "text-center p-4 cursor-pointer text-xl block bg-gray-700" : "text-center cursor-pointer p-4 text-xl block "} >{option}</div>
+                    ))}
+                    {currentQuestion<bookFound.questions.length-1 ?
+                    <button className="self-center italic flex-grow-0 w-36 font-bold border border-gray-700 text-gray-700 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:text-white hover:bg-gray-800 focus:outline-none focus:shadow-outline"
+                        onClick={handleChangeQuestion}
+                    >
+                        {t(`quiz.next`)}
+                    </button> :
+                    <button className="self-center italic flex-grow-0 w-36 font-bold border border-gray-700 text-gray-700 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:text-white hover:bg-gray-800 focus:outline-none focus:shadow-outline" onClick={handleFinish}>{t(`quiz.finish`)}</button>}
+                </div>
+            </div>
           </>
         
     );
