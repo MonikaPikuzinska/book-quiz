466f11f585d4a79509d46f62391ca37f11d72b37
MonikaPikuzinska <monika.pikuzinska01@gmail.com>
2021-03-12 19:22:01 +0100

    added download one question and download answer on click

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -12,8 +12,7 @@ function Quiz() {
     const color = useSelector(state => state.color);
     const lang = useSelector(state => state.lang);
 
-    const [choosenBook, setChoosenBook] = useState([]);
-    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [currentQuestion, setCurrentQuestion] = useState(1);
     const [options, setOptions] = useState([]);
     const [questions, setQuestions] = useState('');
     const [answer, setAnswer] = useState(0);
@@ -21,25 +20,24 @@ function Quiz() {
     const [counter, setCounter] = useState(0);
     const [isFinished, setIsFinished] = useState(false);
     const [rightAns, setRightAns] = useState(false);
+    const [quizLen, setQuizLen] = useState(0);
 
-    const API = `http://localhost:3000/${book}`;
-    
+    const API = `http://localhost:3000/${book}/${currentQuestion}`;
+    const API_ans = `http://localhost:3000/${book}_ans/${currentQuestion}`;
+
     const getData = async() => {
         const response = await axios.get(API);
         const data = response.data;
-        setChoosenBook(data);
-        setQuestions(data[currentQuestion].question[lang])
-        setAnswer(data[currentQuestion].answer);
-        setOptions(data[currentQuestion].options[lang])
+        setQuestions(data.question[lang]);
+        setAnswer(data.answer);
+        setOptions(data.options[lang]);
+        setQuizLen(data.all_question);
     };
     
-    const loadQuizData = () => {
-        setQuestions(choosenBook[currentQuestion].question[lang])
-        setAnswer(choosenBook[currentQuestion].answer);
-        setOptions(choosenBook[currentQuestion].options[lang])
-    };
-
-    const handleChangeQuestion = () => {
+    const handleChangeQuestion = async() => {
+        const response = await axios.get(API_ans);
+        const data = response.data;
+        setAnswer(data.answer);
         if(myAnswer===answer){
           setCounter(counter+1);
         }
@@ -48,7 +46,7 @@ function Quiz() {
         opt.forEach(o=>{
             o.classList.remove("bg-green-200")
             o.classList.remove("bg-red-200")
-        })
+        });
       };
 
     const mounted = useRef(false);
@@ -57,25 +55,31 @@ function Quiz() {
             getData();
             mounted.current = true;
         } else {
-            loadQuizData();
+            getData();
         }
     });
 
-    const handleCheckAnswer = (myAnswer, i) => {
+    const handleCheckAnswer = async (myAnswer, i) => {
+        const response = await axios.get(API_ans);
+        const data = response.data;
+        setAnswer(data.answer);
         setMyAnswer(options.indexOf(myAnswer));
-        if (myAnswer===answer){
+        if (myAnswer===data.answer){
             let element = document.getElementById(i);
             element.classList.toggle("bg-green-200");
         } else {
             let element = document.getElementById(i);
             element.classList.toggle("bg-red-200");
-            let element2 = document.getElementById(answer);
+            let element2 = document.getElementById(data.answer);
             element2.classList.toggle("bg-green-200");
         };
     };
 
-    const handleFinish = ()=>{
+    const handleFinish = async()=>{
         setIsFinished(true);
+        const response = await axios.get(API_ans);
+        const data = response.data;
+        setAnswer(data.answer);
     };
 
     return (
@@ -87,12 +91,12 @@ function Quiz() {
                 <button className="italic flex-grow-0 w-36 font-bold border border-gray-700 text-gray-700 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:text-white hover:bg-gray-800 focus:outline-none focus:shadow-outline"><Link to={'/'}>{t(`quiz.main`)}</Link></button>
                 <h2 className={`p-4 text-center self-center block italic font-bold text-8xl text-${color}-400`}>{t(`welcome.${book}`)}</h2>
                 <div className="flex flex-col self-center p-8 m-8 shadow-2xl">
-                    <h2>{currentQuestion +1}  / {choosenBook.length} </h2>
+                    <h2>{currentQuestion}  / {quizLen} </h2>
                     <h1 className="text-center p-2 text-xl block italic font-bold">{questions} </h1>
                     {options.map((option, i)=> (
                     <div id={i} onClick={()=>handleCheckAnswer(option, i)} className={rightAns ? "option text-center p-4 cursor-pointer text-xl block bg-gray-700" : "option text-center cursor-pointer p-4 text-xl block "} >{option}</div>
                     ))}
-                    {currentQuestion<choosenBook.length-1 ?
+                    {currentQuestion<quizLen ?
                     <button className="self-center italic flex-grow-0 w-36 font-bold border border-gray-700 text-gray-700 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:text-white hover:bg-gray-800 focus:outline-none focus:shadow-outline"
                         onClick={handleChangeQuestion}
                     >
