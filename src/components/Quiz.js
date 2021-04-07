38ae538ba65a573a67d8bca34bf488fe5d6c4711
Marta Lazarowicz <marta.lazarowicz9@gmail.com>
2021-04-07 19:38:52 +0200

    set css class for checked answer

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -5,7 +5,6 @@ import { selectAllQuestion, fetchNewQuestion } from '../actions/questionActions'
 import { selectAllAnswer, fetchNewAnswer } from '../actions/answerActions';
 import { API } from './API';
 import { next } from '../actions';
-import { getAnswer } from '../actions/answerActions';
 import { Link } from 'react-router-dom'; 
 import {useTranslation} from "react-i18next";
 
@@ -27,6 +26,9 @@ function Quiz() {
     const [myAnswer, setMyAnswer] = useState(0);
     const [counter, setCounter] = useState(0);
     const [isFinished, setIsFinished] = useState(false);
+    const [answer, setAnswer] = useState(null);
+    const [answeredChecked, setAnswerChecked] = useState(false);
+
 
     const handleChangeQuestion = () => {
         if(rightAns.length>0 && myAnswer===rightAns[0].answer){
@@ -45,32 +47,32 @@ function Quiz() {
     
     useEffect(() => {
         dispatch(fetchNewQuestion(APILink));
-    },[]);
+    },[answer]);
+
     useEffect(() => {
-        dispatch(fetchNewQuestion(APILink_ans));
-        console.log(rightAns);
-        if (rightAns.length>0 && myAnswer===rightAns[0].answer){
-            let element = document.getElementById(myAnswer);
-            element.classList.toggle("bg-green-200");
-        } else if (rightAns.length>0) {
-            let element = document.getElementById(myAnswer);
-            element.classList.toggle("bg-red-200");
-            let element2 = document.getElementById(rightAns[0].answer);
-            element2.classList.toggle("bg-green-200");
-        };
+        async function fetch() {
+            if (myAnswer) {
+                await dispatch(fetchNewAnswer(APILink_ans));
+            }  
+        }
+        fetch();
     },[myAnswer]);
+
     const handleCheckAnswer = (myanswer, i) => {
-        // setMyAnswer(options[0].options[lang].indexOf(myanswer));
+        setAnswerChecked(true);
         setMyAnswer(i);
-        // if (rightAns.length>0 && myAnswer===rightAns[0].answer){
-        //     let element = document.getElementById(i);
-        //     element.classList.toggle("bg-green-200");
-        // } else if (rightAns.length>0) {
-        //     let element = document.getElementById(i);
-        //     element.classList.toggle("bg-red-200");
-        //     let element2 = document.getElementById(rightAns[0].answer);
-        //     element2.classList.toggle("bg-green-200");
-        // };
+    };
+
+    const getCheckedClass = (i) => {
+        if (answeredChecked && rightAns) {
+            if (i === myAnswer) {
+                return rightAns.answer === i ? 'bg-green-200' : 'bg-red-200';
+            } else if (i === rightAns.answer) {
+               return 'bg-green-200';
+            } 
+        } else {
+            return '';
+        }
     };
 
     const handleFinish = ()=>{
@@ -94,7 +96,9 @@ function Quiz() {
                     <h2>{currentQuestion}  / {quizLen[0].all_question} </h2>
                     <h1 className="text-center p-2 text-xl block italic font-bold">{question[0].question[lang]} </h1>
                     {options[0].options[lang].map((option, i)=> (
-                    <div id={i} onClick={()=>handleCheckAnswer(option, i)} className="option text-center cursor-pointer p-4 text-xl block " >{option}</div>
+                    <div id={i} onClick={()=>handleCheckAnswer(option, i)} 
+                    className={`option text-center cursor-pointer p-4 text-xl block ${getCheckedClass(i)}`} >
+                        {option}</div>
                     ))}
                     {currentQuestion<quizLen[0].all_question ?
                         <button className="self-center italic flex-grow-0 w-36 font-bold border border-gray-700 text-gray-700 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:text-white hover:bg-gray-800 focus:outline-none focus:shadow-outline"
