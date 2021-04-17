aa9642f8aa9abc26ce655db808f06980b8c0b3c4
MonikaPikuzinska <monika.pikuzinska01@gmail.com>
2021-04-17 15:50:08 +0200

    deleted selectors

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -19,8 +19,6 @@ function Quiz() {
     const APILink = `${API}/${book}/${currentQuestion}`;
     const APILink_ans = `${API}/${book}_ans/${currentQuestion}`;
     const question = useSelector(state => state.question);
-    const options = useSelector(state => state.question);
-    const quizLen = useSelector(state => state.question);
     const rightAns = useSelector(state => state.answer);
     const [myAnswer, setMyAnswer] = useState(0);
     const [counter, setCounter] = useState(0);
@@ -76,14 +74,14 @@ function Quiz() {
                 <div className="flex flex-col">
                     <h2 className={`p-4 text-center self-center block italic font-bold text-8xl text-${color}-400`}>{t(`welcome.${book}`)}</h2>
                     <div className="flex flex-col self-center p-8 m-8 shadow-2xl">
-                    <h2>{currentQuestion}  / {quizLen[currentQuestion-1].all_question} </h2>
+                    <h2>{currentQuestion}  / {question[currentQuestion-1].all_question} </h2>
                     <h1 className="text-center p-2 text-xl block italic font-bold">{question[currentQuestion-1].question[lang]} </h1>
-                    {options[currentQuestion-1].options[lang].map((option, i)=> (
+                    {question[currentQuestion-1].options[lang].map((option, i)=> (
                     <div id={i} onClick={()=>handleCheckAnswer(option, i)} 
                     className={`ansopt text-center cursor-pointer p-4 text-xl block ${getCheckedClass(i)}`} >
                         {option}</div>
                     ))}
-                    {currentQuestion<quizLen[currentQuestion-1].all_question ?
+                    {currentQuestion<question[currentQuestion-1].all_question ?
                         <button className="self-center italic flex-grow-0 w-36 font-bold border border-gray-700 text-gray-700 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:text-white hover:bg-gray-800 focus:outline-none focus:shadow-outline"
                         onClick={handleChangeQuestion}
                         >
